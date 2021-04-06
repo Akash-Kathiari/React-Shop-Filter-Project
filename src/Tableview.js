6df205d4377a4c77115e6824485d40512f426c30
Akash Kathiari <ak2378@njit.edu>
2021-04-05 20:44:00 -0400

    first commit

---
+++ src/Tableview.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "./App";
+
+function Tableview(props){
+
+        return (<div>Hello i couldn't get this to work :(</div>)
+    }
+    
+    
+export default Tableview;
